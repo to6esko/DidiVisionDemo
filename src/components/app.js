7c171e5914b4834040e8782d8b059ba5e051bb5e
Todor Krastev <to6esko69@gmail.com>
2017-01-08 07:49:51 +0200

    add history and react-router

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+    }
     render() {
         return (
             <div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
+import createHistory from 'history/createBrowserHistory';
 import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+const app = document.getElementById('app');
+ReactDOM.render(
+    <Router history={hashHistory} >
+    <Router path="/" component={App} />   
+    </Router>, app
+);
