6eb4eac02c30099d2224452591951da988d232a1
Todor Krastev <to6esko69@gmail.com>
2017-01-08 18:08:43 +0200

    add logo png

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 export default class Home extends React.Component {
     render() {
+        let logo = {
+            backgroundImage: 'url(' + 'img/Logo-1.png' + ')', width: 180, height: 90};
         return (
-            <div>
-                <div>
-                    <h1>Начало</h1>
-                </div>
-            </div>
+            <div className="logo-content">
+                <span className="logo" style={logo}></span>        
+            </div>  
         );
     }
 }
